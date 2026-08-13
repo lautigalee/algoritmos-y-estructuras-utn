2f6851bcbf0e7a12ca7f6ca97ec086328060b2df
Lautaro Galetto <Lautygale13@gmail.com>
2026-08-13 14:59:11 -0300

    Agrego ejercicio tipo parcial

--- Practica segundo parcial/tipoparcial3.c
+++ Practica segundo parcial/tipoparcial3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+<<<<<<< HEAD
 void cargarMatriz(int n, int m, int matriz[5][6]);
 void mostrarMatriz(int n, int m, int matriz[5][6]);
 void buscarMenorDiagonal(int n, int m, int matriz[5][6]);
@@ -128,4 +129,88 @@ void mostrarVector(int vector[30], int m)
         printf("%d ", vector[i]);
     }
     printf("\n");
+=======
+void CargaMatriz(int f, int c);
+void MenorPromedio(int f, int c);
+int GenerarVector(int f, int c);
+void MostrarVector(int cant);
+void OrdenarVector(int cant);
+int VEC_MAYO_50[30];
+int stock[5][6];
+int main() {
+    srand(time(NULL));
+	int f, c, cant;
+    do {
+        printf("Ingrese la cantidad de depósitos: ");
+        scanf("%d", &f);
+    }
+    while(f <= 0 || f >= 5);
+    do {
+        printf("Ingrese la cantidad de productos: ");
+        scanf("%d", &c);
+    }
+    while(c <= 0 || c >= 6);
+    CargaMatriz(f,c);
+    MenorPromedio(f, c);
+    cant = GenerarVector(f, c);
+    MostrarVector(cant);
+    OrdenarVector(cant);
+    MostrarVector(cant);
+	return 0;
+}
+void CargaMatriz(int f, int c) {
+    int i, j;
+    for(i = 0; i < f; i++) {
+        for(j = 0; j < c; j++) {
+            stock[i][j] = rand() % 100;
+        }
+    }
+}
+void MenorPromedio(int f, int c) {
+int depositoMin = 0;
+float minPromedio = 0.0;
+int i, j;
+for (i = 0; i < f; i++) {
+    float suma = 0;
+    for (j = 0; j < c; j++) {
+        suma += stock[i][j];
+    }
+    float promedioActual = suma / c; 
+    if (i == 0 || promedioActual < minPromedio) {
+        minPromedio = promedioActual;
+        depositoMin = i;
+    }
+}
+printf("\n1) El deposito con menor promedio es el numero %d (promedio: %.2f)\n", depositoMin, minPromedio);
+}
+int GenerarVector(int f, int c) {
+    int i, j, k = 0;
+    for(i = 0; i < f; i++) {
+        for(j = 0; j < c; j++) {
+            if(stock[i][j] > 50) {
+                VEC_MAYO_50[k] = stock[i][j];
+                k++;
+            }
+        }
+    }
+    return k;
+}
+void MostrarVector(int cant) {
+    int i;
+    for(i = 0; i < cant; i++) {
+        printf("%d ", VEC_MAYO_50[i]);
+    }
+}
+void OrdenarVector(int cant) {
+    int i, j, aux;
+    for(i = 0; i < cant-1; i++) {
+        for(j = 0; j < cant-1-i; j++) {
+            if(VEC_MAYO_50[j+1] > VEC_MAYO_50[j]) {
+                aux = VEC_MAYO_50[j];
+                VEC_MAYO_50[j] = VEC_MAYO_50[j+1];
+                VEC_MAYO_50[j+1] = aux;
+            }
+        }
+    }
+>>>>>>> e257c68 (Agrego ejercicio tipo parcial)
 }
